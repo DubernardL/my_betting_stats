b8d357333ec04ba231f75d70a40855a83b7b385e
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-03-02 22:24:59 +0100

    show api ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,37 +5,14 @@ require 'json'
 
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :fixture]
+  include Fetch
+  before_action :set_bets
+  before_action :set_matchs
 
   def home
-    url = URI("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/525/next/10")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-host"] = 'api-football-v1.p.rapidapi.com'
-    request["x-rapidapi-key"] = '44316d2130msh21fed66e06e6a24p1dd597jsnf2e92ca6ac85'
-
-    response = http.request(request)
-    @matchs = JSON.parse(response.read_body)["api"]["fixtures"]
   end
 
   def fixture
-    url = URI("https://api-football-v1.p.rapidapi.com/v2/odds/fixture/157689")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-host"] = 'api-football-v1.p.rapidapi.com'
-    request["x-rapidapi-key"] = '44316d2130msh21fed66e06e6a24p1dd597jsnf2e92ca6ac85'
-
-    response = http.request(request)
-    bookmakers = JSON.parse(response.read_body)["api"]["odds"].first["bookmakers"]
-    unibet = bookmakers.select { |bookmaker| bookmaker.value?("Unibet") }.first
-    @bets = unibet["bets"]
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap"
 
 // React components
-import "../bet"
+// import "../bet"
 
