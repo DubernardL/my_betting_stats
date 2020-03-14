d9cfd1ca7156b940b515d1978c4ef82513293e32
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-03-14 16:17:26 +0100

    home added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,12 @@ require 'openssl'
 require 'json'
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :stats]
 
   def home
+  end
+
+  def stats
     # All bets
     @bets = Bet.where(user: current_user)
 
