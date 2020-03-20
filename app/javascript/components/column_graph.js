6ad14afeb9e08a0031a6659e58586bfde023c354
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-03-20 12:56:58 +0100

    graph amount bet added

---
+++ app/javascript/components/column_graph.js
@@ -0,0 +1,23 @@
+google.charts.load("current", {packages:['corechart']});
+google.charts.setOnLoadCallback(columnChart);
+function columnChart() {
+  var data = google.visualization.arrayToDataTable(column_graph);
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1,
+                   { calc: "stringify",
+                     sourceColumn: 1,
+                     type: "string",
+                     role: "annotation" },
+                   2]);
+
+  var options = {
+    title: "Medium amount bets",
+    bar: {groupWidth: "70%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+  chart.draw(view, options);
+}
+
+export {columnChart};
--- app/javascript/packs/graphs.js
+++ app/javascript/packs/graphs.js
@@ -2,3 +2,4 @@
 import { drawChart } from "../components/circle_graph";
 import { graphPoint } from "../components/points_graph";
 import { drawLogScales } from "../components/line_graph";
+import { columnChart } from "../components/column_graph";
