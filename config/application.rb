910961a535c6fd18481c8d7ae846bbe526437df6
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-03-24 17:28:10 +0100

    remove config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,4 @@ module MyBettingStats
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-  config.assets.initialize_on_precompile = false
 end
